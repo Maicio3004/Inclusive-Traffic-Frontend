98cbaba788473711a2eeed8dbf0d35dea27f32d1
Mauricio Marin <marinmauricio1000@gmail.com>
2025-10-04 14:33:39 -0500

    Setup lucide icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { Camera } from "lucide-react";
 
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold" >Hello World!</h1>
       <Button className="bg-gray-700 text-white" >Click me</Button>
+      <Camera color="black" size={48} />
     </div>
   );
 }
